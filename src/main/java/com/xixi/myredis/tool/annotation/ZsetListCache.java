ad1bc964a27ed12bb40dbfec95bfc7bd0c26a1cf
shengchengchao <shengchengchao@ewell.cc>
2021-03-28 21:53:43 +0800

    docs(redisBase) @ZsetListCache 注解添加注释

--- src/main/java/com/xixi/myredis/tool/annotation/ZsetListCache.java
+++ src/main/java/com/xixi/myredis/tool/annotation/ZsetListCache.java
@@ -7,7 +7,8 @@ import java.lang.annotation.Target;
 
 /**
  * @author shengchengchao
- * @Description
+ * @Description 该注解主要是用于数据的分页，使用zset来进行数据的存储，
+ * 目前仍然存在一个问题 如果redis和数据库存在不同步的情况，无法进行及时的处理 需要保证来数据进入到数据库的同时，同时能够加入到redis中
  * @createTime 2021/3/26
  */
 @Retention(RetentionPolicy.RUNTIME)
